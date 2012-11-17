daaf551e12598c4039ff465002b0bddba1341104
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-11-17 18:52:58 +0400

    Compression tests.

--- tests/Main.hs
+++ tests/Main.hs
@@ -22,6 +22,7 @@ main = do
   tests = TestList
     [ TestLabel "basic" basic
     , TestLabel "advanced" advanced
+    , TestLabel "compressed" compressed
     ]
 
 
@@ -104,6 +105,45 @@ advanced = TestCase $ do
       directory "tar" $ do
         file "s" "test1\n"
         file "t" "test2\n"
+    file_ ".biegunka.biegunka-tar-test"
+
+
+-- | Test compressed .tar handling
+-- Assumes ~/tar directory does exist
+compressed ∷ Test
+compressed = TestCase $ do
+  w ← getHomeDirectory
+
+  -- Uncompress and unpack gzipped tar archive and check layout is correct
+  helper w bgz l []
+  -- Delete everything
+  helper w b' l' [DirectoryDoesNotExist "./test", FileDoesNotExist "./.biegunka.biegunka-tar-test"]
+  -- Uncompress and unpack bzipped tar archive and check layout is correct
+  helper w bbz2 l []
+  -- Delete everything
+  helper w b' l' [DirectoryDoesNotExist "./test", FileDoesNotExist "./.biegunka.biegunka-tar-test"]
+ where
+  bgz =
+    tar_ "http://budueba.com/biegunka-tar-test.tar.gz" "tar/test"
+  bbz2 =
+    tar_ "http://budueba.com/biegunka-tar-test.tar.bz2" "tar/test"
+  l = do
+    directory "tar" $
+      directory "test" $ do
+        directory "x" $ do
+          directory "y" $ do
+            file "s" "test1\n"
+            file "t" "test2\n"
+          directory "z" $
+            file_ "v"
+        directory "w" $
+          file_ "u"
+        file_ "q"
+    file_ ".biegunka.biegunka-tar-test"
+
+  b' = return ()
+  l' = do
+    directory_ "test"
     file_ ".biegunka.biegunka-tar-test"
 
 
